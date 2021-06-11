0810370d044c6d0185bfdc4c57919423d2ba5b43
KristiyanX0 <49171975+KristiyanX0@users.noreply.github.com>
2021-06-12 00:00:40 +0300

    Add files via upload

--- Hangman Game/Hangman Game/repl.h
+++ Hangman Game/Hangman Game/repl.h
@@ -13,9 +13,7 @@ class repl {
 public:
 	void run();
 private:
-	unsigned int v;
 	unsigned int word_count = getword_count();
-	bool guessed_letters[26];
 	bool letters[26] = {false};
 
 	void mark_containing_letters(const string& str);
@@ -24,15 +22,12 @@ private:
 	void safeTo_file(const string&, const string& crtipted, const string& word, unsigned int v);
 	void open_saved_game(const string& name);
 
-
 	const string cript(const string& word);
 	void addword(string& str);
 	void analyse_command(string &command);
 	void game(const string& name, unsigned v = 0, bool saved = true, string cripted = "", string word = "");
 	void login_command(string& name, string& str);
-	
 	bool is_it_in(char x);
-	
 };
 
 #endif // !REPL_H
